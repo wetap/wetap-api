f4510d6269a1ddb462d68d0f4e39850e91e64af1
Michael Kirk and Sam Vevang <sam.vevang+michael+sam@gmail.com>
2014-03-14 17:00:17 -0400

    heroku's example database configuration for postgis

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,32 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+# Example Heroku congiguration based on
+# https://devcenter.heroku.com/articles/postgis
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password,
+          postgis_extension:   true,
+          schema_search_path:  ['public', 'postgis']
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
+
 module WetapApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
