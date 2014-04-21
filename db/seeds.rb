8ecf344786a5cbcf32faf73b7800d23da3070272
Michael Kirk <michael.john.kirk@gmail.com>
2014-04-21 16:46:24 -0700

    add some waterfountain seeds for testing fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# LA Union Station
+WaterFountain.create!("location" => { "type" => "Point",
+                                      "coordinates" => [34.055, -118.2352] })
+
+# San Francisco MOMA
+WaterFountain.create!("location" => { "type" => "Point",
+                                      "coordinates" => [37.7858, -122.4008] })
+
+
