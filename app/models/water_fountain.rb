2afbce13d90dd859df4f527ab4eb0dcdd1704882
Sam Vevang <sam.vevang@gmail.com>
2014-04-22 14:44:12 -0400

    handle input that is not a dictionary

--- app/models/water_fountain.rb
+++ app/models/water_fountain.rb
@@ -5,7 +5,8 @@ class WaterFountain < ActiveRecord::Base
   scope :bounded_by, ->(bbox_params) { where("ST_Intersects(location, ST_MakeEnvelope(?, 4326))", bbox_params) }
 
   def location=(val)
-    self[:location] = RGeo::GeoJSON.decode(val.deep_stringify_keys).to_s
+    val = val.deep_stringify_keys if val.class == Hash
+    self[:location] = RGeo::GeoJSON.decode(val).to_s
   end
 
   def location
