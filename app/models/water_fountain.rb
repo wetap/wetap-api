bbfa5149b0b90d49f1a27af4bca6915f06168f24
Sam Vevang <sam.vevang@gmail.com>
2014-04-22 08:47:18 -0400

    deep_stringify the geojson dictionary

--- app/models/water_fountain.rb
+++ app/models/water_fountain.rb
@@ -5,7 +5,7 @@ class WaterFountain < ActiveRecord::Base
   scope :bounded_by, ->(bbox_params) { where("ST_Intersects(location, ST_MakeEnvelope(?, 4326))", bbox_params) }
 
   def location=(val)
-    self[:location] = RGeo::GeoJSON.decode(val).to_s
+    self[:location] = RGeo::GeoJSON.decode(val.deep_stringify_keys).to_s
   end
 
   def location
