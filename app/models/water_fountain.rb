750995bf010a8b0a1417fae828463c87baded2b2
Sam Vevang <sam.vevang@gmail.com>
2014-04-30 10:37:03 -0400

    handle idl bounding box in eastern hemisphere

--- app/models/water_fountain.rb
+++ app/models/water_fountain.rb
@@ -27,7 +27,16 @@ class WaterFountain < ActiveRecord::Base
 
   def self.bbox_crosses_dateline(bbox_params)
     return false unless bbox_params.class == Array && bbox_params.count == 4
-    bbox_params[0] > bbox_params[3]
+
+    lhs = bbox_params[0]
+    rhs = bbox_params[2]
+
+    lhs_in_eastern = lhs >= 0 && rhs <= 180
+    rhs_in_western = rhs <= 0 && rhs >= -180
+
+    # TODO, because we derive the bbox from the google map api viewport
+    # we don't handle the case e.g in the eastern hemi that lhs > rhs
+    lhs_in_eastern && rhs_in_western
   end
 
 end
