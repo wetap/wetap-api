01acac42687181cb5a3ebd039d22a81ff0321472
Ian Hall <ianh.99@gmail.com>
2014-05-09 17:46:04 -0700

    use fancy lat/lon getters

--- app/assets/angular/services/models.js
+++ app/assets/angular/services/models.js
@@ -2,18 +2,18 @@
 
 var app = angular.module('app');
 
-app.factory('WaterFountain', ['$resource', function($resource) {
+app.factory('WaterFountain', ['$resource', function ($resource) {
 
     var waterFountainFactory = $resource('/water_fountains/:id', {id: '@id'});
-    
-    waterFountainFactory.prototype.longitude = function(){
+
+    waterFountainFactory.prototype.longitude = function () {
       return this.location.coordinates[0];
     };
 
-    waterFountainFactory.prototype.latitude = function(){
+    waterFountainFactory.prototype.latitude = function () {
       return this.location.coordinates[1];
     };
-    
+
     return waterFountainFactory;
   }]
 );
