bad2a74d2758c39da581af78643a565d54319f15
ianhall <ianh.99@gmail.com>
2014-05-10 17:26:34 -0700

    update routing spec to handle new default response format of json

--- spec/routing/water_fountains_routing_spec.rb
+++ spec/routing/water_fountains_routing_spec.rb
@@ -4,31 +4,31 @@ describe WaterFountainsController do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/water_fountains").to route_to("water_fountains#index")
+      expect(:get => "/water_fountains").to route_to(:format => :json, :controller => "water_fountains", :action => "index")
     end
 
     it "routes to #new" do
-      expect(:get => "/water_fountains/new").to route_to("water_fountains#new")
+      expect(:get => "/water_fountains/new").to route_to(:format => :json, :controller => "water_fountains", :action =>"new")
     end
 
     it "routes to #show" do
-      expect(:get => "/water_fountains/1").to route_to("water_fountains#show", :id => "1")
+      expect(:get => "/water_fountains/1").to route_to(:format => :json, :controller => "water_fountains", :action =>"show", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/water_fountains/1/edit").to route_to("water_fountains#edit", :id => "1")
+      expect(:get => "/water_fountains/1/edit").to route_to(:format => :json, :controller => "water_fountains", :action =>"edit", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/water_fountains").to route_to("water_fountains#create")
+      expect(:post => "/water_fountains").to route_to(:format => :json, :controller => "water_fountains", :action =>"create")
     end
 
     it "routes to #update" do
-      expect(:put => "/water_fountains/1").to route_to("water_fountains#update", :id => "1")
+      expect(:put => "/water_fountains/1").to route_to(:format => :json, :controller => "water_fountains", :action =>"update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/water_fountains/1").to route_to("water_fountains#destroy", :id => "1")
+      expect(:delete => "/water_fountains/1").to route_to(:format => :json, :controller => "water_fountains", :action =>"destroy", :id => "1")
     end
 
   end
