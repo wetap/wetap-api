7505b0909f2fee834648122caf63d9df023bc0ae
Sam Vevang <sam.vevang@gmail.com>
2014-05-17 17:13:19 -0400

    force correct aws host name

--- app/models/water_fountain.rb
+++ app/models/water_fountain.rb
@@ -3,7 +3,9 @@ class WaterFountain < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
                             :storage => :s3,
-                            :s3_credentials => 'config/aws-credentials.yml'
+                            :s3_credentials => 'config/aws-credentials.yml',
+                            :s3_host_name => "s3-us-west-2.amazonaws.com"
+
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
