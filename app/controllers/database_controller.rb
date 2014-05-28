bcf260e44dc95d112d1198cccbb1db4dc39e4378
Michael Kirk and Sam Vevang <michael.john.kirk+michael.john.kirk+sam.vevang@gmail.com>
2014-05-28 11:46:59 -0700

    need absolute path when server is daemonized

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -2,7 +2,7 @@ class DatabaseController < ApplicationController
   def reset
     if Rails.env.test?
       DatabaseCleaner.clean_with :truncation
-      load 'db/seeds.rb'
+      load Rails.root + 'db/seeds.rb'
       render text: "reset the database"
     else
       raise RuntimeError.new("tried to reset Database in non-test Environment")
