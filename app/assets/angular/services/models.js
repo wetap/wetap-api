3410193eb1baf6e4ae8872e93f03a9ef94911642
Ian Hall <ianh.99@gmail.com>
2014-05-28 18:54:34 -0700

    refactor WaterFountain factory prototype to provide properties called "longitude" and "latitude"

--- app/assets/angular/services/models.js
+++ app/assets/angular/services/models.js
@@ -6,13 +6,14 @@ weTap.factory('WaterFountain', ['$resource', function ($resource) {
 
   var waterFountainFactory = $resource('/water_fountains/:id.json', {id: '@id'});
 
-  waterFountainFactory.prototype.longitude = function () {
-    if (angular.isDefined(this.location)) {return this.location.coordinates[0];}
-  };
-
-  waterFountainFactory.prototype.latitude = function () {
-    if (angular.isDefined(this.location)) {return this.location.coordinates[1];}
-  };
+  // define properties to allow the app to use, for example, wf.latitude instead of wf.location.coordinates[1]
+  _.each(["longitude", "latitude"], function(coordinate, ix) {
+      Object.defineProperty(waterFountainFactory.prototype, coordinate, {
+        get: function () {return this.location.coordinates[ix];},
+        enumerable : true,
+        configurable : true
+      });
+    });
 
   return waterFountainFactory;
 }]);
