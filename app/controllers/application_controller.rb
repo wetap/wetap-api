b62432bec868eea7c49113a295c123400857c6f9
Sam Vevang <sam.vevang@gmail.com>
2014-05-30 15:25:09 -0400

    after login, send users to special api auth resource admin users are sent to the admin namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   respond_to :json
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.admin?
+      admin_root_path
+    else
+      request.env['omniauth.origin'] || stored_location_for(resource) || water_fountains_after_login_path
+    end
+  end
+
 end
