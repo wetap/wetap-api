4a76b79e15e97bc82b9b3434326ce2016116d9c0
Sam Vevang <sam.vevang@gmail.com>
2014-05-30 16:32:49 -0400

    create a new controller for a placeholder root page

---
+++ app/controllers/home_controller_controller.rb
@@ -0,0 +1,4 @@
+class HomeControllerController < ApplicationController
+  def home
+  end
+end
---
+++ spec/views/home_controller/home.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "home_controller/home.html.haml" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
