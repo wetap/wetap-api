a5a935de7aa08cafd6969207ea80a532d3b30bdd
Michael Kirk <michael.john.kirk@gmail.com>
2014-05-30 15:57:01 -0700

    test environment needs to look for .env to work for calabash

--- config/initializers/s3_credentials.rb
+++ config/initializers/s3_credentials.rb
@@ -1,14 +1,5 @@
-if Rails.env.test?
-  # So that tests pass when ENV isn't set on ci
-  S3_CREDENTIALS = {
-    access_key_id: 'fake-access-key-id', 
-    secret_access_key: 'fake-secret-access-key',
-    bucket: 'fake-aws-s3-bucket'
-  }
-else
-  S3_CREDENTIALS = {
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket: ENV['AWS_S3_BUCKET']
-  }
-end
+S3_CREDENTIALS = {
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'] || 'fake-access-key-id',
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || 'fake-secret-access-key',
+  bucket: ENV['AWS_S3_BUCKET'] || 'fake-aws-s3-bucket'
+}
