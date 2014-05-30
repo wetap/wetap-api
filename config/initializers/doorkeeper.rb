0eaac97db8cfc7e6606f3164ab91678e829803de
Sam Vevang <sam.vevang@gmail.com>
2014-05-30 17:56:26 -0400

    doorkeeper skips authorization

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -75,9 +75,9 @@ Doorkeeper.configure do
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
   # For example if dealing with trusted a application.
-  # skip_authorization do |resource_owner, client|
-  #   client.superapp? or resource_owner.admin?
-  # end
+  skip_authorization do |resource_owner, client|
+     true
+  end
 
   # WWW-Authenticate Realm (default "Doorkeeper").
   # realm "Doorkeeper"
