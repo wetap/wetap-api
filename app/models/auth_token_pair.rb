6a66cb38453939b3d5909461cf13517de9434758
Sam Vevang <sam.vevang@gmail.com>
2014-06-02 17:09:05 -0400

    helper methods

--- app/models/auth_token_pair.rb
+++ app/models/auth_token_pair.rb
@@ -5,12 +5,16 @@ class AuthTokenPair < ActiveRecord::Base
     return user.auth_token_pair if user.auth_token_pair.present?
 
     auth = new
-    auth.private_token = AuthTokenPair.friendly_token_for(:private_token)
-    auth.public_token = AuthTokenPair.friendly_token_for(:public_token)
+    auth.generate_tokens!
     auth.user = user
     user.auth_token_pair = auth
     auth.save
     return auth
+  end
+
+  def generate_tokens!
+    self.private_token = AuthTokenPair.friendly_token_for(:private_token)
+    self.public_token = AuthTokenPair.friendly_token_for(:public_token)
   end
 
   def self.friendly_token_for(attr)
