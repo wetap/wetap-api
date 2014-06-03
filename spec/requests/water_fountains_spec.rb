42525b3a672f8dc81a5bec06c44a3627a5ef10c1
Sam Vevang <sam.vevang@gmail.com>
2014-06-03 18:07:08 -0400

    users are only authorized across one request

--- spec/requests/water_fountains_spec.rb
+++ spec/requests/water_fountains_spec.rb
@@ -7,4 +7,20 @@ describe "WaterFountains" do
       expect(response.status).to be(200)
     end
   end
+
+  describe "when supplying a token" do
+
+    let(:user) { User.new(email: "test@foo.com", password:"12345678") }
+    let(:public_token) { user.public_token }
+    let(:valid_attributes) { { "location" => {"type" => "Point","coordinates" => [1.0,1.0]} } }
+    before { user.save }
+
+    it "signs in a user for only one request" do
+      post water_fountains_path(format: :json, public_token: public_token, water_fountain: valid_attributes)
+      expect(response.status).to be(201)
+      post water_fountains_path(format: :json, water_fountain: valid_attributes)
+      expect(response.status).to be(401)
+    end
+  end
+
 end
