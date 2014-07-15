c4fdb5d749a197fb8fa74603dc380632cc1b1798
Michael Kirk <michael.john.kirk@gmail.com>
2014-07-14 18:00:05 -0700

    configure sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,15 @@ WetapApi::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'wetap.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
