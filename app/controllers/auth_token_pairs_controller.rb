f6007c59cc2da39e8c97ac10dd43f2004df8e4f7
Sam Vevang <sam.vevang@gmail.com>
2014-07-30 10:50:05 -0400

    58bf78a redux, send users to the auth_token_pairs

--- app/controllers/auth_token_pairs_controller.rb
+++ app/controllers/auth_token_pairs_controller.rb
@@ -2,7 +2,6 @@ class AuthTokenPairsController < ApplicationController
 
   def me
     if !user_signed_in?
-      session['user_return_to']
       return redirect_to user_session_path
     end
   end
